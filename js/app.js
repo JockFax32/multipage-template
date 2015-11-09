beed66caf620f9a324c30b9e59a36a58f531ae15
JockFax32 <foxtjack@gmail.com>
2015-11-09 09:32:42 -0800

    Multipage

--- js/app.js
+++ js/app.js
@@ -1,13 +1,36 @@
 // Create app
-
+var myApp = angular.module('myApp', ['ui.router'])
 // Configure app
+myApp.config(function($stateProvider){
+    $stateProvider
+        .state('home', {
+            url:'/',
+            templateUrl: 'templates/home.html',
+            controller: 'HomeController',
+        }).state('content',{
+            url:'/content',
+            templateUrl:'templates/content.html',
+            controller: 'ContentController',
+        }).state('about',{
+            url:'/about',
+            templateUrl:'templates/about.html',
+            controller:'AboutController'
+        })
+})
 
 // Landing page controller: define $scope.number as a number
-
+.controller('HomeController',function($scope){
+    $scope.number= 24;
+})
 
 // About page controller: define $scope.about as a string
+.controller('AboutController', function($scope){
+    $scope.about='Expression from Angular';
+})
 
 
 // Content controller: define $scope.url as an image
-
+.controller('ContentController', function($scope){
+    $scope.url = 'https://upload.wikimedia.org/wikipedia/commons/d/df/Fox_study_6.jpg'
+})
 
